f382e1f600868ec243c2a6e821403c36f3f1e105
Kevin Berendsen <info@kevinberendsen.nl>
2018-10-26 14:11:53 +0200

    Remove certain condition in the before install script

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_3ed83aec37b2_key -iv $encrypted_3ed83aec37b2_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
-    gpg --fast-import travis/codesigning.asc
-fi
+openssl aes-256-cbc -K $encrypted_3ed83aec37b2_key -iv $encrypted_3ed83aec37b2_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+gpg --fast-import travis/codesigning.asc
