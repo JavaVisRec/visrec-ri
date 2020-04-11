983c78a9021fe708deddf794809eb6919352a485
kevto <berendsen.kevin@gmail.com>
2020-04-11 12:05:07 +0200

    Removing ignored files from index and removing travis related files.

--- travis/before-install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_3ed83aec37b2_key -iv $encrypted_3ed83aec37b2_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
-gpg --fast-import travis/codesigning.asc
