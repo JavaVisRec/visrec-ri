4b5de28738e975ebf927fc9442c22ff9a3872629
Zoran Sevarac <sevarac@gmail.com>
2021-01-19 20:31:54 +0100

    Changed File to Path params

--- src/main/java/javax/visrec/ri/ml/classification/ImageClassifierNetwork.java
+++ src/main/java/javax/visrec/ri/ml/classification/ImageClassifierNetwork.java
@@ -1,22 +1,12 @@
 package javax.visrec.ri.ml.classification;
 
 import deepnetts.data.ExampleImage;
-import deepnetts.data.ImageSet;
 import deepnetts.net.ConvolutionalNetwork;
-import deepnetts.net.train.BackpropagationTrainer;
-import deepnetts.net.train.opt.OptimizerType;
-import deepnetts.util.DeepNettsException;
-import deepnetts.util.FileIO;
 
 import javax.visrec.AbstractImageClassifier;
-import javax.visrec.util.VisRecConstants;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Implementation of abstract image classifier for BufferedImage-s using
@@ -47,8 +37,8 @@ public class ImageClassifierNetwork extends AbstractImageClassifier<BufferedImag
         // get all class labels with corresponding output larger then classification threshold
         Map<String, Float> results = new HashMap<>();
         for (int i = 0; i < outputs.length; i++) {
-          //  if (outputs[i] > getThreshold())
-            results.put(neuralNet.getOutputLabel(i), outputs[i]); // if : threshold_            
+           if (outputs[i] >= getThreshold())
+                results.put(neuralNet.getOutputLabel(i), outputs[i]);         
         }
 
         return results;
@@ -62,88 +52,4 @@ public class ImageClassifierNetwork extends AbstractImageClassifier<BufferedImag
         return inputHeight;
     }
 
-    // static builder method for this class
-    public static javax.visrec.util.Builder<ImageClassifierNetwork> builder() {
-        return new Builder();
-    }
-
-    @Deprecated
-    public static class Builder implements javax.visrec.util.Builder<ImageClassifierNetwork> {
-
-        private final Logger LOGGER = Logger.getLogger(ImageClassifierNetwork.class.getName());
-
-        @Override
-        public ImageClassifierNetwork build() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public ImageClassifierNetwork build(Map<String, Object> config) {
-
-
-            int imageWidth = Integer.parseInt(String.valueOf(config.get(VisRecConstants.IMAGE_WIDTH)));
-            int imageHeight = Integer.parseInt(String.valueOf(config.get(VisRecConstants.IMAGE_HEIGHT)));
-            String labelsFile = String.valueOf(config.get(VisRecConstants.LABELS_FILE));
-            String trainingFile = String.valueOf(config.get(VisRecConstants.TRAINING_FILE));
-            float maxError = Float.parseFloat(String.valueOf(config.get(VisRecConstants.SGD_MAX_ERROR)));
-            int maxEpochs = Integer.parseInt(String.valueOf(config.get(VisRecConstants.SGD_MAX_EPOCHS)));
-            float learningRate = Float.parseFloat(String.valueOf(config.get(VisRecConstants.SGD_LEARNING_RATE)));
-            // subSampleSize u procentima ili tacan broj?
-            // invert zero mean
-
-            String saveToFile = String.valueOf(config.get(VisRecConstants.MODEL_SAVE_TO));
-
-            ImageSet imageSet = new ImageSet(imageWidth, imageHeight);
-            LOGGER.info("Loading images...");
-
-            imageSet.loadLabels(new File(labelsFile));
-            try {
-                imageSet.loadImages(new File(trainingFile), 1000); // paths in training file should be relative
-                // imageSet.invert(); // donw while loading and set flag
-                //  imageSet.zeroMean();
-                imageSet.shuffle();
-            } catch (DeepNettsException ex) {
-                java.util.logging.Logger.getLogger(ImageClassifierNetwork.class.getName()).log(Level.SEVERE, null, ex);
-                return null;
-            }
-
-            LOGGER.info("Done!");
-            LOGGER.info("Creating neural network...");
-
-            String modelJsonFile = String.valueOf(config.get("visrec.model.deepnetts"));
-            ConvolutionalNetwork neuralNet = null;
-            try {
-                neuralNet = (ConvolutionalNetwork) FileIO.createFromJson(new File(modelJsonFile));
-                neuralNet.setOutputLabels(imageSet.getTargetNames()); // redirektuj na getTargetNames
-            } catch (IOException ex) {
-                Logger.getLogger(ImageClassifierNetwork.class.getName()).log(Level.SEVERE, null, ex);
-                return null;
-            }
-
-            LOGGER.info("Training neural network");
-
-            neuralNet.setOutputLabels(imageSet.getTargetNames());
-
-            // create a set of convolutional networks and do training, crossvalidation and performance evaluation
-            BackpropagationTrainer trainer = neuralNet.getTrainer();
-            trainer.setLearningRate(learningRate)
-                    .setMaxError(maxError)
-                    .setMaxEpochs(maxEpochs)
-                    .setBatchMode(false)
-                    .setOptimizer(OptimizerType.SGD);
-            trainer.train(imageSet);
-
-            ImageClassifierNetwork dnImgClassifier = new ImageClassifierNetwork(neuralNet);
-
-            try {
-                FileIO.writeToFile(neuralNet, saveToFile);
-            } catch (IOException ex) {
-                java.util.logging.Logger.getLogger(ImageClassifierNetwork.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            return dnImgClassifier;
-
-        }
-
-    }
 }
--- src/main/java/javax/visrec/ri/ml/classification/ZeroRuleClassifier.java
+++ src/main/java/javax/visrec/ri/ml/classification/ZeroRuleClassifier.java
@@ -1,19 +1,79 @@
 package javax.visrec.ri.ml.classification;
 
+import deepnetts.data.MLDataItem;
 import java.util.Map;
 import javax.visrec.ml.classification.Classifier;
+import javax.visrec.ml.data.DataSet;
 
 
-public class ZeroRuleClassifier<T, R> implements Classifier<T, Map<R, Float>> {
+/**
+ * Zero rule classifier always returns as a result the most frequent class from the data set.
+ * It is used for benchmarking purposes: if a model performs worse than that, then it is useless.
+ * 
+ */
+public class ZeroRuleClassifier<T, R> implements Classifier<T, Map<String, Float>> {
 
+    String mostFrequentClass;
+    
     @Override
-    public Map<R, Float> classify(T input) {
-        // always return the most frequent class     
-        return null;
+    public Map<String, Float> classify(T input) {
+        return Map.of(mostFrequentClass, 1.0f);
     }
     
     // TODO: Add builder
+    // builder chould simply cont the occurance of all possible classes in the data set
 
+    public static ZeroRuleClassifierBuilder builder() {
+        return new ZeroRuleClassifierBuilder();
+    }
 
+    public static class ZeroRuleClassifierBuilder<T> {
+        ZeroRuleClassifier buildingBlock;
+        
+        
+        public ZeroRuleClassifierBuilder() {
+            buildingBlock = new ZeroRuleClassifier();
+        }
+        
+        public ZeroRuleClassifierBuilder trainingSet(DataSet<MLDataItem> dataSet) {
+            int[] classCount = new int[dataSet.getTargetNames().length];
+            for(MLDataItem ml : dataSet.getItems()) {
+                float[] cols = ml.getTargetOutput().getValues();
+                for(int i=0; i<cols.length; i++) {
+                    if (cols[i]==1) classCount[i]++;
+                }
+            }
+            
+            int maxIdx = indexOfMax(classCount);
+            //ZeroRuleClassifier.this.mostFrequentClass =  dataSet.getTargetNames()[maxIdx];
+            // uzmo koa je to klasa
 
+            // count occurancies of each class
+            // how to know which attibutes are classes. are thay targets? golumns MLDataSetItem
+            // napravi ovde brojac
+            
+                // kako da izbrojim klase sa obektm o 
+                // moram da znam tip elemenata u dat asetu - ali ovaj korisiti deep netts implemetaciju?            
+              //dataSet.getTargetNames();
+              //dataSet.getColumnNames();
+          //    dataSet.getTargetColumns();
+          //    dataSet.columnAt(4).setAsTarget(true);
+              //dataSet.setTargetColumns(4, 5, 6);
+              // cognitive load of a programming line - nuber of concept, understandability
+                    
+            return this;
+        }
+
+        private int indexOfMax(int[] classCount) {
+            int max = classCount[0], maxIdx=0;
+            for(int i=1; i<classCount.length; i++) {
+                if (classCount[i] > max) {
+                    max = classCount[i];
+                    maxIdx = i;
+                }
+            }
+            return maxIdx;
+        }
+        
+    }    
 }
